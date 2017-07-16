9246e5d57abe350a60a52634f508983942dfdd30
navneet <hello_navneet@hotmail.com>
2017-07-16 08:39:31 +0530

    Using HostBinding to bind DOM property to reactively change by HostListener, removes uses of Renderer2 and ElementRef.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,21 +1,25 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  // constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  constructor() { }
 
   ngOnInit() {
    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#b3caf0');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#b3caf0');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '#b3caf0');
+    this.backgroundColor = '#b3caf0';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
